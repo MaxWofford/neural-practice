d0b81e4d6fb26bf6501c37f67bef501db81589bc
Max Wofford <max@maxwofford.com>
2016-09-18 03:58:33 -0400

    Add bias to neuron.py

--- neuron.py
+++ neuron.py
@@ -4,13 +4,16 @@ from math import *
 class Neuron():
     def __init__(self, size, alpha):
         self.alpha = alpha
-        self.weights = [random() for _ in range(size)]
+        self.weights = [random() for _ in range(size + 1)]
 
     def feed_forward(self, inputs):
+        inputs.append(1)
         return self.activate(sum([x*y for x, y in zip(inputs, self.weights)]))
 
     def activate(self, x):
-        return 1 / (1 + (e**(-x)))
+        x = min(500, x) # Python doesn't like numbers bigger than this
+        x = max(-500, x)
+        return 1 / (1 + (pow(e, -x)))
 
     def train(self, inputs, desired_output):
         output = self.feed_forward(inputs)
@@ -23,8 +26,14 @@ def check(x, y):
 
 n = Neuron(2, 0.01)
 
-for x in [[random() * 21 - 10, uniform(-10, 10)] for _ in range(1000000)]:
+for x in [[uniform(-100, 100), uniform(-100, 100)] for _ in range(1000000)]:
     n.train(x, check(x[0], x[1]))
+
 print(n.feed_forward([1, 3])) # Should be about 0.5
+print(n.feed_forward([1, 3.1])) # Should be about 1
+print(n.feed_forward([1, 2.9])) # Should be about 0
+print(n.feed_forward([-1000, -3000])) # Should be about 0.5
+print(n.feed_forward([-1000, -3001])) # Should be about 0
+print(n.feed_forward([-1000, -2999])) # Should be about 1
 print(n.feed_forward([1, 4])) # Should be about 1
 print(n.feed_forward([1, 2])) # Should be about 0
